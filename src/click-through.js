8e2d078ce22af560e343285c7f2f4ed9a90032b2
jleonard <thejohnleonard@gmail.com>
2013-08-20 08:34:48 -0400

    init

---
+++ src/click-through.js
@@ -0,0 +1,17 @@
+(function($){ "use strict";
+
+	var ClickThrough = function(el, options){
+		this.$el = $(el);
+		this.options = options;
+	}
+
+	ClickThrough.DEFAULTS = {
+    loop: false,
+    imgs: []
+  }
+
+  ClickThrough.prototype.advance = function(e){
+  	
+  }
+
+})(window.jQuery);
